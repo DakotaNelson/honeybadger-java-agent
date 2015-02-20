c675d340325b02e49f6576420a7d2c1d5c2e8c90
Ethan Robish <ethan.robish@gmail.com>
2015-02-20 02:55:26 -0600

    Making the serve script more robust.

--- serve.sh
+++ serve.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-(cd output && python -m SimpleHTTPServer &)
+echo "Shutting down Apache."
+sudo service apache2 stop
+(cd output && sudo python -m SimpleHTTPServer 80 &)
 echo "Now starting payload listeners. Please be patient."
 msfconsole -r output/listeners.rc
