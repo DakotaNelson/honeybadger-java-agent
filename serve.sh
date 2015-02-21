4280a70de97cd6f7aa01e6a3c269e6fee1d52b77
Ethan Robish <ethan.robish@gmail.com>
2015-02-20 23:49:51 -0600

    Attempt at more graceful serve.sh.

--- serve.sh
+++ serve.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo "Shutting down Apache."
+echo "Shutting down Apache..."
 sudo service apache2 stop
 (cd output && sudo python -m SimpleHTTPServer 80 &)
+serverPID="$!"
 echo "Now starting payload listeners. Please be patient."
 msfconsole -r output/listeners.rc
+
+# When msfconsole shuts down we also want to kill the python server
+echo "Shutting down python web server..."
+kill -INT "$serverPID"
+echo "You will need to restart Apache."
